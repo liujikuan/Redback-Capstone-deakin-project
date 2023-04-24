afc699b51a930a5980a764d8191cd3e75d5c090b
jikuan liu <liujikuan.job@gmail.com>
2023-04-24 15:04:08 +1000

    add another API which verifies the user's TOTP

--- T1_2023/Project 2 2-Factor Authentication (2FA)/code/src/main/java/config/MainApplication.java
+++ T1_2023/Project 2 2-Factor Authentication (2FA)/code/src/main/java/config/MainApplication.java
@@ -23,37 +23,44 @@ public class MainApplication {
         System.out.println(barcodeurl);
         Redback_2FAConfig.createQRCode(barcodeurl, fileAddr, 400, 400);//find the picture in your root directory
 
-        setSecretKey(secretKey);
+        setSecretKey(email, secretKey);
         return fileAddr;
     }
 
-    private static String getSecretKey() {
+    private static String getSecretKey(String email) {
+        //TODO: retrieve the key in a database according to the email
         return secretKey;
     }
 
-    private static void setSecretKey(String key) {
+    private static void setSecretKey(String email, String key) {
+        //TODO:  store the username and the key in a database
         secretKey = key;
     }
 
     private static String secretKey = null;
 
     public static void main(String[] args) throws IOException, WriterException {
-        get2FAbarcode("testing@gmail.com");
-        String secretKey = getSecretKey();
+        String email = "testing@gmail.com";
+        get2FAbarcode(email);
         System.out.println(new Timestamp(System.currentTimeMillis()));
         System.out.println("Please enter the 6 digit code:");
         Scanner scanner = new Scanner(System.in);
         String code = scanner.nextLine();
-        System.out.println("the TOTPCode(secretKey) is :"+ getTOTPCode(secretKey));
+        verifyIdentityByOTP(email,code);
+
+    }
+    public static int verifyIdentityByOTP(String email,String code){
+        String OTP = getTOTPCode(getSecretKey(email));
+        System.out.println("the TOTPCode(secretKey) is :"+ OTP);
         System.out.println("the input code is :"+ code);
-        if (code.equals(getTOTPCode(secretKey))) {
+        if (code.equals(OTP)) {
             System.out.println("Logged in successful");
+            return 0;
         } else {
             System.out.println("Invalid 2FA Code. Please try again.");
+            return 1;
         }
 
     }
-
-
 }
 
